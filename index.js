5cfaf18c5af1177f51f4318d7b473bd1d50608a6
kas-lipko <kas.lipko@yahoo.com>
2021-02-04 16:01:02 +0100

    cart update

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
 const app = express();
 
 app.use(express.static('public'));
@@ -12,8 +15,11 @@ app.use(
     keys: ['lkasld235j']
   })
 );
+
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
